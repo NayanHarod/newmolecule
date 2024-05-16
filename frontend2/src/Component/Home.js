3cd3b282c8d6cc4dbe43f12819bd35a6a0962e1c
Nayan361 <nayanh@linkites.com>
2024-05-16 16:15:33 +0530

    Change the Home page And name of Project

--- frontend2/src/Component/Home.js
+++ frontend2/src/Component/Home.js
@@ -4,21 +4,25 @@ import gemini from './gemin.json'
 import Lottie from "lottie-react";
 import GlassForm from '../GlassForm';
 import { Link } from 'react-router-dom';
+import Mind from './MMind.json'
 
 const Home = () => {
   return (
     <div className='mainclass'>
+<div className='Home123'>
+<Lottie animationData={Mind}/> 
+</div>
 
-    <div className='Home1'> Welcome to <span className='gred'>Lunar-pot</span></div>
-    
-    <div className='gred21'>Diabetes Evaluation and Expert Prognosis</div>
-    <Link to={'/vacc'} ><button className='b165'>Try Deep-AI</button></Link>
+    <div className='Home1'> Welcome to <span className='gred'>Molecule-Mind</span></div>
+
+    <div className='gred21'>Connecting AI to Cure...</div>
+    <Link to={'/VAcc'} ><button className='b165'>Continue...</button></Link>
     
 
     
-      <div>
+      <div className='anm'>
       <Lottie animationData={gemini}/>  
-      </div>
+      </div>  
    
 
 
